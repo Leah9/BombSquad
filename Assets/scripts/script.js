fce9a8fb5bdc1e7504188eb1a134c570d90280f7
Leah C <101465290+Leah9@users.noreply.github.com>
2022-10-26 20:53:46 +0100

    Basic layout

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,32 +1,44 @@
 console.log("Script linked ok!");
 
-let dictEasy = ['cat', 'dog', 'ball', 'foot', 'hand', 'head', 'rat', 'bag', 'sad', 'pat', 'leg', 'arm', 'car', 'bus', 'train'];
+let dictEasy = ['CAT', 'DOG', 'BALL', 'FOOT', 'HAND', 'HEAD', 'RAT', 'bag', 'sad', 'pat', 'leg', 'arm', 'car', 'bus', 'train'];
 let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 let lettersTried = [];
 let attemptsRemaining = 15;
+let lettersCorrect = 0;
 
 console.log(alphabet);
-let word = dictEasy[Math.floor(Math.random()*dictEasy.length)];
+let word = dictEasy[Math.floor(Math.random() * dictEasy.length)];
 console.log(word);
 let wordLength = word.length;
 console.log(wordLength);
 
 $('#word-display').empty();
 
-for (let i = 0; i < word.length; i++){
+for (let i = 0; i < word.length; i++) {
     $('#word-display').append('_ ');
     console.log(i);
 }
 
-for(let i = 0; i < alphabet.length; i++){
+for (let i = 0; i < alphabet.length; i++) {
     $('#keyboard').append(`<div id="${alphabet[i]}" class="keyboard">${alphabet[i]}</div>`);
 }
 
-$('.keyboard').click(function(){
+$('.keyboard').click(function () {
     console.log(this.id)
+    
+    for (letter in word) {
+        console.log(letter + word)
+        if (word[letter] == this.id) {
+            console.log('CORRECT')
+            $(this).addClass('green');
+            lettersCorrect ++;
+
+        }
+    }
+    
     lettersTried.push(this.id)
     console.log(lettersTried);
-
+    $(this).addClass('red');
 })
 
 
