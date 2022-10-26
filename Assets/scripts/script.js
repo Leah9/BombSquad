2044fcbc4f534774abaf7fe5502a56c092fe770f
Leah C <101465290+Leah9@users.noreply.github.com>
2022-10-26 21:59:36 +0100

    Displaying guessed letters correctly

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,36 +1,58 @@
 console.log("Script linked ok!");
 
+// Set up array of words for easy mode
 let dictEasy = ['CAT', 'DOG', 'BALL', 'FOOT', 'HAND', 'HEAD', 'RAT', 'BAG', 'SAD', 'PAT', 'LEG', 'ARM', 'CAR', 'BUS', 'TRAIN'];
+
+// Set up array to contain our alphabet, this allows us to create the keyboard
 let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-let lettersTried = [];
-let attemptsRemaining = 15;
-let lettersCorrect = 0;
+let lettersTried = []; // Array of letters that have been tried
+let attemptsRemaining = 15; // Guesses remaining
+let lettersCorrect = 0; // How manu letters have been guessed correctly
 
 console.log(alphabet);
+// Choose a random word from the dictEasy array
 let word = dictEasy[Math.floor(Math.random() * dictEasy.length)];
 console.log(word);
 let wordLength = word.length;
 console.log(wordLength);
 
 $('#word-display').empty();
+let wordDisplay = [];
 
+// 
 for (let i = 0; i < word.length; i++) {
-    $('#word-display').append('_ ');
+    wordDisplay.push('_')
     console.log(i);
 }
 
+$('#word-display').text(wordDisplay);
+console.log(wordDisplay);
+
 for (let i = 0; i < alphabet.length; i++) {
     $('#keyboard').append(`<div id="${alphabet[i]}" class="keyboard">${alphabet[i]}</div>`);
+}
+
+function updateWordDisplay()
+{
+    $('#word-display').text('');
+    for(letter in wordDisplay)
+    {
+        $('#word-display').append(wordDisplay[letter]);
+    }
+   
 }
 
 $('.keyboard').click(function () {
     console.log(this.id)
     
     for (letter in word) {
-        console.log(letter + word)
+        //console.log(letter + word)
         if (word[letter] == this.id) {
             console.log('CORRECT')
             $(this).addClass('green');
+            wordDisplay[letter] = this.id;
+            console.log(wordDisplay);
+            updateWordDisplay();
             lettersCorrect ++;
 
         }
